3f344451342fa7cae71c34af2c1730431de479c0
zeestack <zhussain@ualberta.net>
2021-12-16 21:49:07 -0700

    feat: Django Authentication System

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,3 @@
-from typing import Reversible
-
 from django.contrib import admin, messages
 from django.contrib.admin.filters import SimpleListFilter
 from django.contrib.admin.views.main import ChangeList
@@ -111,7 +109,8 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "membership", "order_count"]
     list_editable = ["membership"]
     list_per_page = 10
-    ordering = ["first_name", "last_name"]
+    list_select_related = ["user"]
+    ordering = ["user__first_name", "user__last_name"]
     search_fields = ["first_name__istartswith", "last_name__istartswith"]
 
     @admin.display(ordering="order_count")
--- storefront/settings.py
+++ storefront/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "store",
     "tags",
-    "store_custom",
     "likes",
+    "core",
 ]
 
 STATIC_URL = "/static/"
@@ -150,3 +150,5 @@ REST_FRAMEWORK = {
     # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     # "PAGE_SIZE": 10,
 }
+
+AUTH_USER_MODEL = "core.User"
